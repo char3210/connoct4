1288cb416d4f58d317208a3075440d16f07f74ba
BuildTools <unconfigured@null.spigotmc.org>
2020-12-10 19:06:19 -0800

    initial commit pt. 2

---
+++ bot.py
@@ -0,0 +1,94 @@
+import os
+import discord
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+import c4
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+bot = commands.Bot(command_prefix='c!')
+
+p1=p2=None
+nums = ['1\uFE0F\u20E3']
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected')
+
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    
+    if user.bot:
+        return
+
+    global board
+    if (reaction.message==board)==False:
+        return
+    
+    global p1, p2
+    if p1==None:
+        p1=user
+        await reaction.message.channel.send(f"Player 1 has joined: {user.name} ({c4.red})")
+    elif p2==None:
+        p2=user
+        await reaction.message.channel.send(f"Player 2 has joined: {user.name} ({c4.yellow})")
+
+    
+    try:
+        chosen = int(reaction.emoji[0])-1
+    except:
+        return
+
+    if (user == p1 and c4.currentPiece == c4.red) or (user==p2 and c4.currentPiece == c4.yellow):
+        if not c4.place(c4.cols[chosen]):
+                await reaction.message.channel.send("invalid move, next to go is still "+c4.currentPiece)
+    else :
+        await reaction.message.channel.send("It is not your turn!")
+
+    
+    await reaction.remove(user)
+    await board.edit(content = c4.getboard())
+    
+    if c4.checkwin('<:red:588903539926106112>'):
+        await reaction.message.channel.send(f"red ({p1.name}) wins!")
+        stopgame()
+    elif c4.checkwin('<:yellow:588903561149153280>'):
+        await reaction.message.channel.send(f"yellow ({p2.name}) wins!")
+        stopgame()
+    elif c4.checkdraw():
+        await reaction.message.channel.send("the game has drawn!")
+        stopgame()
+    
+
+@bot.command(name='start')
+async def start(ctx):
+    stopgame()
+    global board
+    board = await ctx.send(c4.getboard())
+    for x in range(1,8):
+        await board.add_reaction(f'{x}\uFE0F\u20E3')
+
+@bot.command(name='stop')
+async def stopcmd(ctx):
+    stopgame()
+    await ctx.send('game has been stopped')
+
+def stopgame():
+    global p1, p2, board
+    c4.clear()
+    p1=None
+    p2=None
+    board = None
+
+@bot.command(name='resend')
+async def resendBoard(ctx):
+    global board
+    await board.delete()
+    board = await ctx.send(c4.getboard())
+    for x in range(1,8):
+        await board.add_reaction(f'{x}\uFE0F\u20E3')
+    
+bot.run(TOKEN)
---
+++ c4.py
@@ -0,0 +1,86 @@
+
+blank = '<:black:588903518912380939>'
+red = '<:red:588903539926106112>'
+yellow = '<:yellow:588903561149153280>'
+
+currentPiece = red
+c1=[blank]*6
+c2=[blank]*6
+c3=[blank]*6
+c4=[blank]*6
+c5=[blank]*6
+c6=[blank]*6
+c7=[blank]*6
+cols=[c1,c2,c3,c4,c5,c6,c7]
+
+def place(col):
+    i = len(col)-1
+    while i>=0:
+        if col[i]==blank:
+            col[i]=currentPiece
+            togglePiece()
+            return True
+        i=i-1
+    return False
+
+def togglePiece():
+    global currentPiece
+    if currentPiece == red:
+        currentPiece = yellow
+    else :
+        currentPiece = red
+
+def getboard():
+    res = f'Turn: {currentPiece}\n \n'
+    for i in range(len(c1)):
+        for j in range(len(cols)):
+            res += cols[j][i]
+        res += '\n'
+    return res
+
+
+def checkwin(char):
+    #check column win
+    for i in range(len(cols)):
+        col=cols[i]
+        for j in range(len(c1)-3):
+            if (col[j]==char and col[j+1]==char and
+                col[j+2]==char and col[j+3]==char):
+                return True
+        
+    #check row win
+    for i in range(len(c1)):
+        for j in range(len(cols)-3):
+            if (cols[j][i]==char and cols[j+1][i]==char and
+                cols[j+2][i]==char and cols[j+3][i]==char) :
+                return True
+        
+
+    #check diagonal win
+    for i in range(len(c1)-3):
+        for j in range(len(cols)-3):
+            if ( (cols[j][i]==char and cols[j+1][i+1]==char and
+                  cols[j+2][i+2]==char and cols[j+3][i+3]==char) or
+                 (cols[len(cols)-j-1][i]==char and cols[len(cols)-j-2][i+1]==char and
+                  cols[len(cols)-j-3][i+2]==char and cols[len(cols)-j-4][i+3]==char) ):
+                return True
+
+def checkdraw():
+    #check draw
+    for i in range(len(cols)):
+        if cols[i][0] == blank:
+            return False
+    return True
+
+
+
+def clear():
+    global cols, currentPiece
+    for col in cols:
+        for p in range(len(col)):
+            col[p]=blank
+    
+    currentPiece = red
+
+
+
